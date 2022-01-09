1b4e609fc74886a2ea1e4349efe30dff3be0763e
Mo Saeed Nahlous <jay.haji041@gmail.com>
2022-01-09 13:36:29 +0200

    Saving user data in local storage

--- frontend/src/apiCalls.js
+++ frontend/src/apiCalls.js
@@ -1,17 +1,27 @@
-import axios from 'axios'
+import axios from 'axios';
 export const loginCall = async (userCredentials, dispatch) => {
-    dispatch({ type: 'LOGIN_REQUEST' })
-    try {
-        const { data } = await axios.post('/auth/login', userCredentials)
-        
-        dispatch({
-            type: 'LOGIN_SUCCESS',
-            payload: data
-        })
-    } catch (error) {
-        dispatch({
-            type: 'LOGIN_FAIL',
-            payload: error
-        })
-    }
-}
+  dispatch({ type: 'LOGIN_REQUEST' });
+  try {
+    const { data } = await axios.post('/auth/login', userCredentials);
+    dispatch({
+      type: 'LOGIN_SUCCESS',
+      payload: data,
+    });
+    localStorage.setItem(
+      'user',
+      JSON.stringify({
+        _id: data._id,
+        username: data.username,
+        email: data.email,
+        profilePic: data.profilePic,
+        coverPic: data.coverPic,
+        isAdmin: data.isAdmin,
+      })
+    );
+  } catch (error) {
+    dispatch({
+      type: 'LOGIN_FAIL',
+      payload: error,
+    });
+  }
+};
